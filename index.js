aa4d91a58cabb58d7479b4a7f7ee1f890b6d96c5
FlowerbeanAnsh <ansh.saxena_cs19@gla.ac.in>
2022-09-21 19:02:00 +0530

    Project files created..

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'bf3cf04552msh2972ad3072436adp1771d8jsn478315caba36',
-		'X-RapidAPI-Host': 'youtube-music-downloader.p.rapidapi.com'
+		'X-RapidAPI-Host': 'youtube-dl4.p.rapidapi.com'
 	}
 };
 
-fetch('https://youtube-music-downloader.p.rapidapi.com/mp3?url=https://music.youtube.com/watch?v=B4FZrrg7hZM', options)
+fetch('https://youtube-dl4.p.rapidapi.com/fc8c5416b9cfd8fc?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DBddP6PYo2gs%26list%3DRDdz3aCNEAVtk%26index%3D3%26ab_channel%3DSonyMusicIndia', options)
 	.then(response => response.json())
-	.then(response => console.log(response))
+	.then(response => console.log(response.formats[0].url))
 	.catch(err => console.error(err));
